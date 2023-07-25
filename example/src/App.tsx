9e2a162aaf7f4272f5bfdc8f3b00fe7635b79298
Josh Chen <josh.chen@aiello.ai>
2023-07-25 17:15:39 +0800

    feat: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,17 +3,29 @@ import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SelectableTextView } from 'react-native-selectable-text';
 
+const sentences = [
+  { start_time: 0, end_time: 10, content: 'this is first sentence', index: 1 },
+  { start_time: 11, end_time: 15, content: " and im' second", index: 2 },
+];
+
 export default function App() {
   return (
-    <View style={styles.container} onLayout={(event) => {
-      console.log(event.nativeEvent.layout)
-    }}>
+    <View
+      style={styles.container}
+      onLayout={(event) => {
+        console.log(event.nativeEvent.layout);
+      }}
+    >
       <SelectableTextView
         menuItems={['Comment', 'Height']}
-        onSelection={event => {
-          console.log(event.nativeEvent)
+        playingIndex={1}
+        playingColor="#dfe8ff"
+        textColor="#667280"
+        onSelection={(event) => {
+          console.log('onSelection: ', event.nativeEvent);
         }}
-        value="123fsd asdf afasfasdfassa  asdfa dassafas ss s  f"
+        onClick={(event) => console.log('onClick: ', event.nativeEvent)}
+        sentences={sentences}
         style={styles.box}
         fontSize="16"
       />
@@ -24,16 +36,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#0c9',
-    // flexWrap: "wrap"
   },
   box: {
-    // flex: 1,
-    // flexWrap: "wrap"
-    // width: '50%',
-    // width: "100%",
-    // height: 50,
+    backgroundColor: 'rgba(255, 255, 255, .1)',
+    height: 60,
   },
 });
